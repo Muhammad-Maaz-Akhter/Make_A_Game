3ec6e6f69de09f6c6fb9a8d9eaeb8ca851d1ef28
Maaz <maazakhter19991@gmail.com>
2022-07-29 14:31:30 +0500

    first commit game

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float RotateSpeed=100f;
+    public float jump=8f;
+    bool IsFalling = true;
+    
+    
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotate = Input.GetAxis("Horizontal") * RotateSpeed;
+        rotate *= Time.deltaTime;
+        rb.AddRelativeTorque(Vector3.back * rotate);
+        
+        if (Input.GetKeyDown(KeyCode.W) && IsFalling==true)
+        {
+            IsFalling = false;
+            rb.AddForce(new Vector3(0f, jump, 0f),ForceMode.Impulse);
+            
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            IsFalling=true;
+        }
+
+    }
+
+
+}
